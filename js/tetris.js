75f604d803474d051076ee18201880b60249b1e0
Wlochp20 <91265023+Wlochp20@users.noreply.github.com>
2021-10-21 16:05:38 +0200

    Add files via upload

--- js/tetris.js
+++ js/tetris.js
@@ -12,12 +12,12 @@ btn[0].addEventListener("click", function () {
   }, predkosc);
 });
 p = 4;
-predkosc=300;
+predkosc = 100;
 let figury;
 let rzad;
 let k;
 let rand;
-
+let bok;
 
 
 function render() {
@@ -36,20 +36,28 @@ function render() {
       let cd1 = document.createElement("div");
       document.getElementsByTagName("div")[oblicz].appendChild(cd1);
       szuk[oblicz1].setAttribute("class", "kwadrat pole");
-      // szuk[oblicz1].classList.add('kwadrar', 'pole')
-      // console.log(document.querySelectorAll("div").length);
+      
+     
     }
   }
   stworzblok();
 }
-dodawanie=0
 
 function ruch(e) {
   console.log(e)
-  if(e.key=="ArrowRight"){
+  if(e.key=="ArrowUp"){
+    wyswietl("none");
+    bok++;
+    if(bok>=(figury[rand].length-1)){
+      bok=0
+    }
+    
+  }
+
+  if (e.key == "ArrowRight") {
     p++
     wyswietl("none");
-  }else if(e.key=="ArrowLeft" ){
+  } else if (e.key == "ArrowLeft") {
     p--
     wyswietl("none");
   }
@@ -57,18 +65,13 @@ function ruch(e) {
   dekfig();
   wyswietl(figury[rand][(figury[rand].length - 1)][0]);
 }
-document.addEventListener("keydown",ruch);
+document.addEventListener("keydown", ruch);
+
 function stworzblok() {
   p = 4;
- rzad = 10;
- rand = Math.floor(Math.random() * 4);
-
-
-//  if(dalej1!="none"){
-//   console.log("123")
-// }
-
-
+  bok=0;
+  rzad = 10;
+  rand = Math.floor(Math.random() * 4);
 
   dekfig();
   wyswietl(figury[rand][(figury[rand].length - 1)][0]);
@@ -80,43 +83,43 @@ function stworzblok() {
 
 
 function wyswietl(kolorek) {
-  
+  x=0
   for (r = 0; r < 4; r++) {
     document.getElementsByClassName("kwadrat")[
-      figury[rand][0][r]
+      (figury[rand][bok][r])
     ].style.background = kolorek;
-    
-    if (figury[rand][0][r] > 190) {
+
+    if (figury[rand][bok][r] > 190) {
       rzad = 0;
-      // for(i=0;i<4;i++){
-      //   document.getElementsByClassName("kwadrat")[figury[rand][0][i]].setAttribute("class","polee")
-      // }
-      stworzblok()
+      x++
+      if(x==1){
+        setTimeout(console.log("tak"),0)
+        setTimeout(stworzblok,0)
+      }
+
+      
     }
-    dalej=figury[rand][0][3]
-    if(dalej+10<190){
-      dalej1=document.getElementsByClassName("kwadrat")[dalej+10].style.background
-      if(dalej1!='none')
-         console.log("nic")
-      // if(dalej1=="none"){
-      //   rzad = 0;
-      // }
-    }
-    // console.log(figury[rand][0][r]);
   }
 }
 
 function dekfig() {
   figury = [
-    [[p, p + 10, p + 20, p + 30], [p, p + 1, p + 2, p + 3], ["red"]],
+    [
+      [p, p + 10, p + 20, p + 30],
+      [p, p + 1, p + 2, p + 3],
+      ["red"]
+    ],
     [
       [p, p + 1, p + 2, p + 10 + 1],
       [p + 1, p + 10 + 1, p + 10, p + 20 + 1],
       [p + 10, p + 11, p + 1, p + 12],
-      [p + 1, p + 11, p + 10 + 3, p + 21],
+      [p + 1, p + 11, p + 10 + 2, p + 21],
       ["orange"],
     ],
-    [[p, p + 1, p + 10, p + 11], ["aqua"]],
+    [
+      [p, p + 1, p + 10, p + 11],
+      ["aqua"]
+    ],
     [
       [p + 1, p + 11, p + 21, p + 22],
       [p, p + 1, p + 2, p + 10],
@@ -131,8 +134,16 @@ function dekfig() {
       [p, p + 1, p + 2, p + 12],
       ["pink"],
     ],
-    [[p + 1, p + 2, p + 10, p + 11], [p, p + 10, p + 11, p + 21], ["blue"]],
-    [[p, p + 1, p + 11, p + 12], [p + 1, p + 11, p + 10, p + 20], ["green"]],
+    [
+      [p + 1, p + 2, p + 10, p + 11],
+      [p, p + 10, p + 11, p + 21],
+      ["blue"]
+    ],
+    [
+      [p, p + 1, p + 11, p + 12],
+      [p + 1, p + 11, p + 10, p + 20],
+      ["green"]
+    ],
   ];
 
 }
