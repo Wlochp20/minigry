af216659669a0b85b136829557555b0b2febc1fa
Wlochp20 <91265023+Wlochp20@users.noreply.github.com>
2021-10-22 20:52:23 +0200

    Update tetris.js
    blocks fall on each other

--- js/tetris.js
+++ js/tetris.js
@@ -4,21 +4,26 @@ btn[0].addEventListener("click", function () {
   btn[1].style.display = "none";
   document.getElementsByTagName("select")[0].style.display = "none";
   render();
-  setInterval(() => {
+  inter=setInterval(() => {
     p += rzad;
     wyswietl("none");
     dekfig();
-    wyswietl(figury[rand][(figury[rand].length - 1)][0]);
+    wyswietl(figury[rand][figury[rand].length - 1][0]);
   }, predkosc);
 });
 p = 4;
-predkosc = 100;
+predkosc = 200;
 let figury;
 let rzad;
 let k;
 let rand;
 let bok;
 
+function odswiez(){
+  wyswietl("none");
+  dekfig();
+  wyswietl(figury[rand][figury[rand].length - 1][0]);
+}
 
 function render() {
   let szuk = document.getElementsByTagName("div");
@@ -35,80 +40,78 @@ function render() {
       let oblicz1 = document.querySelectorAll("div").length;
       let cd1 = document.createElement("div");
       document.getElementsByTagName("div")[oblicz].appendChild(cd1);
-      szuk[oblicz1].setAttribute("class", "kwadrat pole");
-      
-     
+      szuk[oblicz1].setAttribute("class", "kwadrat");
     }
   }
   stworzblok();
 }
 
 function ruch(e) {
-  console.log(e)
-  if(e.key=="ArrowUp"){
+  if (e.key == "ArrowUp") {
     wyswietl("none");
     bok++;
-    if(bok>=(figury[rand].length-1)){
-      bok=0
+    if (bok >= figury[rand].length - 1) {
+      bok = 0;
     }
-    
+  }
+  if(e.key=="ArrowDown"){
+    predkosc=100;
+    clearInterval()
   }
 
   if (e.key == "ArrowRight") {
-    p++
+    p++;
     wyswietl("none");
   } else if (e.key == "ArrowLeft") {
-    p--
+    p--;
     wyswietl("none");
   }
 
   dekfig();
-  wyswietl(figury[rand][(figury[rand].length - 1)][0]);
+  wyswietl(figury[rand][figury[rand].length - 1][0]);
 }
 document.addEventListener("keydown", ruch);
 
 function stworzblok() {
   p = 4;
-  bok=0;
+  bok = 0;
   rzad = 10;
   rand = Math.floor(Math.random() * 4);
-
   dekfig();
-  wyswietl(figury[rand][(figury[rand].length - 1)][0]);
-
+  wyswietl(figury[rand][figury[rand].length - 1][0]);
 }
 
-
-
-
-
 function wyswietl(kolorek) {
-  x=0
+  x = 0;
   for (r = 0; r < 4; r++) {
     document.getElementsByClassName("kwadrat")[
-      (figury[rand][bok][r])
+      figury[rand][bok][r]
     ].style.background = kolorek;
-
-    if (figury[rand][bok][r] > 190) {
+    let jak = figury[rand][bok][r];
+    if (
+      figury[rand][bok][r] > 189 ||
+      document
+        .getElementsByClassName("kwadrat")
+        [jak + 10].classList.contains("zajete")
+    ) {
       rzad = 0;
-      x++
-      if(x==1){
-        setTimeout(console.log("tak"),0)
-        setTimeout(stworzblok,0)
-      }
+      x++;
 
-      
+      if (x == 1) {
+        for (i = 0; i < 4; i++) {
+          document
+            .getElementsByClassName("kwadrat")
+            [figury[rand][bok][i]].setAttribute("class", " kwadrat zajete");
+        }
+        setTimeout(stworzblok, 0);
+      }
     }
   }
 }
 
 function dekfig() {
   figury = [
-    [
-      [p, p + 10, p + 20, p + 30],
-      [p, p + 1, p + 2, p + 3],
-      ["red"]
-    ],
+    [[p, p + 10, p + 20, p + 30], [p, p + 1, p + 2, p + 3], ["red"]],
     [
       [p, p + 1, p + 2, p + 10 + 1],
       [p + 1, p + 10 + 1, p + 10, p + 20 + 1],
@@ -116,10 +119,7 @@ function dekfig() {
       [p + 1, p + 11, p + 10 + 2, p + 21],
       ["orange"],
     ],
-    [
-      [p, p + 1, p + 10, p + 11],
-      ["aqua"]
-    ],
+    [[p, p + 1, p + 10, p + 11], ["aqua"]],
     [
       [p + 1, p + 11, p + 21, p + 22],
       [p, p + 1, p + 2, p + 10],
@@ -134,16 +134,7 @@ function dekfig() {
       [p, p + 1, p + 2, p + 12],
       ["pink"],
     ],
-    [
-      [p + 1, p + 2, p + 10, p + 11],
-      [p, p + 10, p + 11, p + 21],
-      ["blue"]
-    ],
-    [
-      [p, p + 1, p + 11, p + 12],
-      [p + 1, p + 11, p + 10, p + 20],
-      ["green"]
-    ],
+    [[p + 1, p + 2, p + 10, p + 11], [p, p + 10, p + 11, p + 21], ["blue"]],
+    [[p, p + 1, p + 11, p + 12], [p + 1, p + 11, p + 10, p + 20], ["green"]],
   ];
-
 }
