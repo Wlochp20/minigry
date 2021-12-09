a65274ddc43a55ab3317ea3e12680f2b6e43664d
Wlochp20 <91265023+Wlochp20@users.noreply.github.com>
2021-12-09 16:04:36 +0100

    Add files via upload

--- js/kolkokrzyzyk.js
+++ js/kolkokrzyzyk.js
@@ -1,25 +1,34 @@
 let losowa;
-for (let index = 0; index < 3; index++) {
-    document.querySelector("body").appendChild(document.createElement("div"))
-    document.querySelectorAll("div")[document.querySelectorAll("div").length-1].setAttribute("class","rzad")
-    for (let index1= 0; index1 < 3; index1++) {
-        document.querySelectorAll(".rzad")[index].appendChild(document.createElement("div"))
-        document.querySelectorAll("div")[document.querySelectorAll("div").length-1].addEventListener("click",graj)
-        document.querySelectorAll("div")[document.querySelectorAll("div").length-1].setAttribute("class","kwadrat")
-        console.log("tak")
-    }
-    function graj() {
-        this.setAttribute("class","kwadrat zajete")
-        for (let index2 = 0; index2 < 9; index2++) {
-            if (document.querySelectorAll(".kwadrat")[index2].classList.contains("zajete")==false) {
-                przeciwnik()
-                break;
-            }
-            
+document.querySelector("button").addEventListener("click",function() {
+    render()
+})
+function render() {
+    document.querySelector("button").style.display="none"
+
+    for (let index = 0; index < 3; index++) {
+        document.querySelector("body").appendChild(document.createElement("div"))
+        document.querySelectorAll("div")[document.querySelectorAll("div").length-1].setAttribute("class","rzad")
+        for (let index1= 0; index1 < 3; index1++) {
+            document.querySelectorAll(".rzad")[index].appendChild(document.createElement("div"))
+            document.querySelectorAll("div")[document.querySelectorAll("div").length-1].addEventListener("click",graj)
+            document.querySelectorAll("div")[document.querySelectorAll("div").length-1].setAttribute("class","kwadrat")
+            console.log("tak")
         }
-       
-       
-    }
+        function graj() {
+            this.setAttribute("class","kwadrat zajete")
+            for (let index2 = 0; index2 < 9; index2++) {
+                if (document.querySelectorAll(".kwadrat")[index2].classList.contains("zajete")==false) {
+                    
+                    przeciwnik()
+                    break;
+                }
+                
+            }
+           
+           
+        }
+}
+
     function przeciwnik() {
         losowa=Math.floor((Math.random() * 9))
         while (document.querySelectorAll(".kwadrat")[losowa].classList.contains("zajete")) {
@@ -27,9 +36,14 @@ for (let index = 0; index < 3; index++) {
           console.log(losowa)
         }
         console.log(losowa)
-        document.querySelectorAll(".kwadrat")[losowa].setAttribute("class","kwadrat zajete");
-        document.querySelectorAll(".kwadrat")[losowa].style.background="green";
+        setTimeout(() => {
+            document.querySelectorAll(".kwadrat")[losowa].setAttribute("class","kwadrat zajete");
+            document.querySelectorAll(".kwadrat")[losowa].style.background="blue";
+        }, 200);
+    }
+    function czywyg() {
         
     }
+
 
 }    
