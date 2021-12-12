56428d693a9b65ff21e0b055154330325f885c9f
Wlochp20 <91265023+Wlochp20@users.noreply.github.com>
2021-12-12 21:01:57 +0100

    Update kolkokrzyzyk.js

--- js/kolkokrzyzyk.js
+++ js/kolkokrzyzyk.js
@@ -4,6 +4,8 @@ let p=1;
 let ruch=[];
 let wygrane=[];
 let ruchprzeciwnika=[];
+let twoj="kolko";
+let przeciwnika="krzyz";
 let wygrana=[
     [1,2,3],
     [1,4,7],
@@ -14,14 +16,31 @@ let wygrana=[
     [3,5,7],
     [3,6,9]
 ]
+document.querySelector(".kolko").style.border="5px solid rgb(0, 225, 255)"
+document.querySelector(".krzyz").addEventListener('click',function () {
+    twoj="krzyz"
+    przeciwnika="kolko"
+    document.querySelector(".krzyz").style.setProperty('--color', "rgb(0, 225, 255)")
+    document.querySelector(".kolko").style.border="5px solid whitesmoke"
+})
+document.querySelector(".kolko").addEventListener('click',function () {
+    twoj="kolko"
+    przeciwnika="krzyz"
+    document.querySelector(".krzyz").style.setProperty('--color', "whitesmoke")
+    this.style.border="5px solid rgb(0, 225, 255)"
+})
 
 document.querySelector("button").addEventListener("click",function() {
     render()
 })
+
+
 function render() {
-    document.querySelector("select").style.display="none"
+    // document.querySelector("select").style.display="none"
     // document.querySelectorAll("button")[1].style.display="none"
+    document.querySelector(".krzyz").style.setProperty('--color', "whitesmoke")
     document.querySelector("button").style.display="none"
+    document.querySelector(".wybor").style.display="none"
     plansza=document.createElement("div")
     document.querySelector("body").appendChild(plansza)
     plansza.className="plansza"
@@ -46,7 +65,7 @@ function graj() {
     this.setAttribute("class","kwadrat zajete")
     kolko=document.createElement("div")
     this.appendChild(kolko)
-    kolko.className="kolko"
+    kolko.className=twoj
     ruch.push(this.id)
     // console.log(this.id)
     czywyg(ruch)
@@ -70,7 +89,7 @@ function przeciwnik() {
         document.querySelectorAll(".kwadrat")[losowa].setAttribute("class","kwadrat zajete");
         krzyzyk=document.createElement("div")
         document.querySelectorAll(".kwadrat")[losowa].appendChild(krzyzyk)
-        krzyzyk.className="krzyz"
+        krzyzyk.className=przeciwnika
         ruchprzeciwnika.push(document.querySelectorAll(".kwadrat")[losowa].id)
      
         czywyg(ruchprzeciwnika)
@@ -84,10 +103,10 @@ function czywyg(jaka) {
     wygrane=[]
     for (let index1 = 0; index1 < wygrana.length; index1++) {
         if (czywygrana==3) {
-            for (let i = 0; i < wygrane.length; i++) {
-                document.querySelectorAll(".kwadrat")[wygrane[i]].style.background="red"
+            // for (let i = 0; i < wygrane.length; i++) {
+            //     document.querySelectorAll(".kwadrat")[wygrane[i]].style.background="red"
 
-            }
+            // }
             setTimeout(() => {
                 document.write("wygrana")
             }, 200);
